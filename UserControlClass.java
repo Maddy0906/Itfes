57fc1c5021dae17105614567ab465c8c0cb6ebe7
Musta <musta@XGP-M.local>
2017-04-19 15:41:35 -0700

    make classes

--- UserControlClass.java
+++ UserControlClass.java
@@ -4,4 +4,28 @@ package Itfes;
  * Created by Nobuaki on 2017-04-19.
  */
 public class UserControlClass {
+
+
+    public void newRegister(){
+        
+    }
+
+    public void rewardAccount(){
+
+    }
+
+    public boolean login(){
+        return true;
+    }
+
+    public String[] getUserEvents(){
+        // DB
+        String[] hoge = new String[1];
+        return hoge;
+    }
+
+    public void setUserEvent(String eventTitle){
+        // DB
+    }
+
 }
