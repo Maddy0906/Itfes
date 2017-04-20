35f045d1dede48d29268453e917b5b42c75d4061
Maddy0906 <madu121@me.com>
2017-04-20 10:44:57 -0700

    adjustment CalendarPage

--- CalendarPage.java
+++ CalendarPage.java
@@ -10,25 +10,24 @@ public class CalendarPage extends DispPageClass {
         System.out.println("This is Calendar Page");
         System.out.println("press allEvent(0) or myEvent(1)");
         System.out.println("menu(99)");
-        Scanner tabMenu = new Scanner(System.in);
-        int num = tabMenu.nextInt();
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
 
         switch (num) {
             case 0: //Display All Event
                 //Call EventControlClass
-                EventControl evCtrl = new evCtrl;
+                EventControlClass evCtrl = new EventControlClass();
                 //Display event information
                 System.out.println(evCtrl.getEventInfo());
                 break;
             case 1: //Display User's Event
                 //Call UseControlClass
-                UseControl usCtrl = new usCtrl;
+                UserControlClass usCtrl = new UserControlClass();
                 //Display event information
-                System.out.println(evCtrl.getUserEventInfo());
+                System.out.println(usCtrl.getUserEventInfo());
                 break;
             case 99://Call Menu
-                public void callMenu () {
-            }
+                this.callMenu();
             default:
                 System.out.println("error");
 
