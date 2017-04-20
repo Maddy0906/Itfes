bbaa9e796c5f1029b26a7371978e0a08fe101280
kaori <kaori55194@gmail.com>
2017-04-20 12:55:08 -0700

    再度カレンダーUP

--- CalendarPage.java
+++ CalendarPage.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 /**
- * Created by madoka on 2017/04/19　これ.
+ * Created by madoka on 2017/04/19.
  */
 public class CalendarPage extends DispPageClass {
 
@@ -20,17 +20,10 @@ public class CalendarPage extends DispPageClass {
         // tabMenu -> scan
         Scanner tabMenu = new Scanner(System.in);
         int num = tabMenu.nextInt();
-    public void dispPage() {
-        System.out.println("This is Calendar Page");
-        System.out.println("press allEvent(0) or myEvent(1)");
-        System.out.println("menu(99)");
-        Scanner scan = new Scanner(System.in);
-        int num = scan.nextInt();
 
         switch (num) {
             case 0: //Display All Event
                 //Call EventControlClass
-
                 //Display event information
                 System.out.println(Arrays.deepToString(ecc.getUpcomingEvents()));
                 break;
@@ -41,19 +34,6 @@ public class CalendarPage extends DispPageClass {
             case 99://Call Menu
                 this.callMenu();
                 break;
-
-                EventControlClass evCtrl = new EventControlClass();
-                //Display event information
-                System.out.println(evCtrl.getEventInfo());
-                break;
-            case 1: //Display User's Event
-                //Call UseControlClass
-                UserControlClass usCtrl = new UserControlClass();
-                //Display event information
-                System.out.println(usCtrl.getUserEventInfo());
-                break;
-            case 99://Call Menu
-                this.callMenu();
             default:
                 System.out.println("error");
 
