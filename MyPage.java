8042abd48e96997c4913cde8f67ecfb5a51d1d00
Maddy0906 <madu121@me.com>
2017-04-20 11:19:15 -0700

    done Mypage and MainPage
    _( :⁍ 」 )_

--- MyPage.java
+++ MyPage.java
@@ -10,72 +10,53 @@ public class MyPage extends DispPageClass {
     public static final String MATCH_MAIL =
             "([a-zA-Z0-9][a-zA-Z0-9_.+\\-]*)@(([a-zA-Z0-9][a-zA-Z0-9_\\-]+\\.)+[a-zA-Z]{2,6})";
 
-    public String dispPage() {
+    public void dispPage() {
         System.out.println("This is My Page");
         System.out.println("press View profile(0) or Edit profile(1)");
         System.out.println("menu(99)");
-        Scanner tabMenu = new Scanner(System.in);
-        int num = tabMenu.nextInt();
+
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
 
         switch (num) {
             case 0: //Display User data(name,E-mail,category,password)
                 //Call UseControlClass
-                UseControl usCtrl = new usCtrl;
+                UserControlClass usCtrl = new UserControlClass();
                 //Display User information
                 System.out.println(usCtrl.getUserDate());
                 break;
-            case 1: //Over ride User data
-                System.out.println("Edit Name(0),E-mail(1),Category(2),Password(3)");
-                Scanner editData = new Scanner(System.in);
-                int editNum = editData.nextInt();
 
-                switch (editNum) {
-                    case 0://Edit Name
-                        System.out.println("Input New Name");
-                        Scanner name = new Scanner(System.in);
-                        String newName = name.nextLine();
-                        //Over ride to UserContorolClass
-                        name.setName = (newName);
-                        return newName;
-                    break;
-                    case 1://Edit E-mail
-                        System.out.println("Input New E-mail");
-                        Scanner email = new Scanner(System.in);
-                        String newEmail = email.nextLine();
-                        //check mail adress
-                        if (newEmail.matches(MATCH_MAIL)) {
-                            System.out.println("true");
-                            //Over ride to UserContorolClass
-                            email.setEmail = (newEmail);
-                            return newEmail;
-                        } else {
-                            System.out.println("error");
-                        }
-                        break;
-                    case 2://Edit Category
-                        System.out.println("Input New Category");
-                        Scanner category = new Scanner(System.in);
-                        String newCategory = category.nextLine();
-                        //Over ride to UserContorolClass
-                        category.setCategory = (newCategory);
-                        return newCategory;
-                    break;
-                    case 3://Edit Password
-                        System.out.println("Input New Password");
-                        Scanner pass = new Scanner(System.in);
-                        String newPass = pass.nextLine();
-                        //Over ride to UserContorolClass
-                        password.setPass = (newPass);
-                        return newPass;
-                    break;
-                    default:
-                        System.out.println("error");
+            case 1: //UserDate RenewAccount()
+                System.out.println("put name");
+                String name = scan.nextLine();
+                System.out.println("put Email");
+                String email = scan.nextLine();
+
+                System.out.println("put password");
+                String password = scan.nextLine();
+                //check mail adress
+                if (email.matches(MATCH_MAIL)) {
+                    System.out.println("okay");
+                } else {
+                    System.out.println("error");
+                }
+
+                //Use UserControlClass RenewAccount()
+                UserControlClass ucc = new UserControlClass();
+                boolean result = ucc.newRegister(name, email, password);
+                if (result) {
+                    // success
+                    System.out.println("Renew account successful");
+                    //Jump to MainPage
+                } else {
+                    // fail
+                    System.out.println("error");
                 }
                 break;
 
             case 99://call menu
-                public void callMenu () {
-            }
+                this.callMenu();
+                break;
 
             default:
                 System.out.println("error");
